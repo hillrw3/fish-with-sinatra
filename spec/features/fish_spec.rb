4fa4a19ae9293f9c730780b0d953060a184d2119
Rob Hill <robert.hill0122@gmail.com>
2014-06-30 15:10:54 -0600

    added link to wiki

--- spec/features/fish_spec.rb
+++ spec/features/fish_spec.rb
@@ -4,13 +4,16 @@ feature "Fish Page" do
   scenario "should have a page header" do
     visit "/"
     click_on "Shark"
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content("Fish Details")
     expect(page).to have_content("Shark")
     expect(page).to have_content("Large predator with big teeth")
-
   end
 
+  scenario "Link to wikipedia from fish name" do
+    visit "/fish/0"
+    expect(page).to have_link("Shark")
+  end
 
 
 end
